5662cb0f349739db0070960ba9569b898509598c
Patryk Mikuli <patryk.mikuli@gmail.com>
2017-08-09 19:58:51 +0200

    Make it work

--- dir-size/Program.cs
+++ dir-size/Program.cs
@@ -1,15 +1,63 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace dir_size
 {
-    class Program
+    internal class Program
     {
-        static void Main(string[] args)
+        private static void Main(string[] args)
         {
+            if (args.Length == 1)
+            {
+                var parentDirectory = args[0];
+                var dirs = Directory.GetDirectories(parentDirectory);
+
+                var result = new Dictionary<string, long>();
+
+                foreach (var dir in dirs)
+                {
+                    var size = GetDirectorySize(dir);
+                    result.Add(dir, size);
+                }
+
+                foreach (var item in result)
+                {
+                    Console.WriteLine(item.Key + " -> " + item.Value + " KiloBytes");
+                }
+            }
+
+#if DEBUG
+            Console.ReadKey();
+#endif
+        }
+
+        private static long GetDirectorySize(string directory)
+        {
+            try
+            {
+                long size = 0;
+                var dirs = Directory.GetDirectories(directory);
+                foreach (var dir in dirs)
+                {
+                    size += GetDirectorySize(dir);
+                }
+
+                var files = Directory.GetFiles(directory);
+                foreach (var file in files)
+                {
+                    size += new FileInfo(file).Length / 1000;
+                }
+
+                return size;
+            }
+            catch (UnauthorizedAccessException)
+            {
+                return 0;
+            }
         }
     }
 }
