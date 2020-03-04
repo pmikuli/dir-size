3bc6e743b365705f6f65bc5006cfab40b0c49cd2
Patryk Mikuli <patryk.mikuli@gmail.com>
2020-03-04 22:07:14 +0100

    Use current directory if no argument is passed.

--- dir-size/Program.cs
+++ dir-size/Program.cs
@@ -14,27 +14,39 @@ namespace dir_size
             if (args.Length == 1)
             {
                 var parentDirectory = args[0];
-                var dirs = Directory.GetDirectories(parentDirectory);
-
-                var result = new Dictionary<string, long>();
-
-                foreach (var dir in dirs)
+                if (Directory.Exists(parentDirectory))
                 {
-                    var size = GetDirectorySize(dir);
-                    result.Add(dir, size);
+                    CalculateSizesAndPrintResults(parentDirectory);
                 }
-
-                var sorted = result.ToList();
-                sorted.Sort((pair1, pair2) => pair1.Value.CompareTo(pair2.Value));
-                foreach (var item in sorted)
-                {
-                    Console.WriteLine(item.Key + " -> " + item.Value + " MegaBytes");
-                }
+            }
+            else if (args.Length == 0)
+            {
+                CalculateSizesAndPrintResults(Directory.GetCurrentDirectory());
             }
 
 #if DEBUG
             Console.ReadKey();
 #endif
+        }
+
+        private static void CalculateSizesAndPrintResults(string parentDirectory)
+        {
+            var dirs = Directory.GetDirectories(parentDirectory);
+
+            var result = new Dictionary<string, long>();
+
+            foreach (var dir in dirs)
+            {
+                var size = GetDirectorySize(dir);
+                result.Add(dir, size);
+            }
+
+            var sorted = result.ToList();
+            sorted.Sort((pair1, pair2) => pair1.Value.CompareTo(pair2.Value));
+            foreach (var item in sorted)
+            {
+                Console.WriteLine(item.Key + " -> " + item.Value + " MegaBytes");
+            }
         }
 
         private static long GetDirectorySize(string directory)
